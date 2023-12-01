c94bdd803e99296550b806fd08618e5f0abb23d6
Alexis Manin <alexis.manin@gmail.com>
2023-12-01 17:09:21 +0100

    Solve day 1
    Both parts

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,44 @@
+val TXT_FIGURES = mapOf(
+    "one"   to "1",
+    "two"   to "2",
+    "three" to "3",
+    "four"  to "4",
+    "five"  to "5",
+    "six"   to "6",
+    "seven" to "7",
+    "eight" to "8",
+    "nine"  to "9"
+)
+
+/**
+ * Find last occurrence of this regex in given input text.
+ * @return `null` if we cannot find any occurrence.
+ */
+fun Regex.findLast(input: CharSequence) = findAll(input).lastOrNull()
+
+/**
+ * Try to fetch value of the first group in this match. Raise an error if it does not exist.
+ */
+fun MatchResult?.firstGroup() = checkNotNull(this?.groupValues?.getOrNull(1)) {
+    "Regex group detection failed. Time to search for a bug !"
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun List<String>.decipher(
+        firstDigitRegex: Regex,
+        lastDigitRegex: Regex?,
+        mappingTable: Map<String, String>?
+    ) : Int = fold(0) {
+        sum, line
+        ->
+        val firstDigit = firstDigitRegex.find(line).firstGroup().let { mappingTable?.get(it) ?: it }
+        val lastDigit = (lastDigitRegex?.find(line) ?: firstDigitRegex.findLast(line)).firstGroup().let { mappingTable?.get(it) ?: it }
+        sum + "$firstDigit$lastDigit".toInt()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) = input.decipher(Regex("(\\d)"), Regex("(\\d)[^\\d]*$"), null)
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part2(input: List<String>) = input.decipher(Regex("(\\d|one|two|three|four|five|six|seven|eight|nine)"), null, TXT_FIGURES)
 
     val input = readInput("Day01")
     part1(input).println()
