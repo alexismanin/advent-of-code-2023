ce71bc918edcbf7527d8ad1952030c9805d2640b
Alexis Manin <alexis.manin@gmail.com>
2023-12-08 22:55:57 +0100

    Utilities: add a function to zip a list of sequences together

--- src/Utils.kt
+++ src/Utils.kt
@@ -28,3 +28,20 @@ fun <T> Sequence<T>.expand(action: (T) -> Sequence<T>) : Sequence<T> {
         sequenceOf(it) + action(it).expand(action)
     }
 }
+
+fun <T> List<Sequence<T>>.zipAll(failIfNotSameSize : Boolean = true) : Sequence<List<T>> {
+    val iters = map { it.iterator() }
+    return sequence {
+        var ended = 0
+        do {
+            val nextBag = ArrayList<T>(iters.size)
+            for (iter in iters) {
+                if (iter.hasNext()) nextBag.add(iter.next())
+                else ended += 1
+            }
+            if (ended == 0) yield(nextBag)
+        } while (ended == 0)
+
+        if (failIfNotSameSize && ended < iters.size) throw IllegalStateException("$ended sequences finished early out of all ${iters.size}")
+    }
+}
